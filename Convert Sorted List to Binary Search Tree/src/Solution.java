b6aec4131bca32451ade18db81db993d09f03fcf
Xiangpeng Li <lxp1991@gmail.com>
2015-07-19 17:36:58 -0400

    Done with Convert Sorted List to Binary Search Tree

--- Convert Sorted List to Binary Search Tree/src/Solution.java
+++ Convert Sorted List to Binary Search Tree/src/Solution.java
@@ -17,9 +17,28 @@
  */
 public class Solution {
     public TreeNode sortedListToBST(ListNode head) {
-        
+        return Helper(head);
     }
-    
+
+    // build bottom-to-top
+    public TreeNode sortedListToBST(int start, int end) {
+        // if finished (root)
+        if(start > end) return null;
+
+        // get mid val
+        int mid = (start + end) / 2;
+
+        // build left sub tree
+        TreeNode left = sortedListToBST(start, mid - 1);
+        // build root node
+        TreeNode root = new TreeNode(head.val);
+        root.left = left;
+        // move to next node to build right sub tree
+        head = head.next;
+        root.right = sortedListToBST(mid + 1, end);
+
+        return root;
+    }
     public static void main(String[] args) {
 		
 	}
