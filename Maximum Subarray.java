7b5d4ddb4f4e9f7cba98e14083da2d77a7d53a4c
Xiangpeng Li <xiangpeng.li@moodys.com>
2017-07-26 14:18:47 -0700

    dp to solve maximum subarray

---
+++ Maximum Subarray.java
@@ -0,0 +1,19 @@
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+    public int maxSubArray(int[] nums) {
+        // write your code
+        if (nums == null || nums.length == 0) return 0;
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = Math.max(sum[i - 1] + nums[i], nums[i]);
+            max = Math.max(max, sum[i]);
+        }
+        return max;
+    }
+}
