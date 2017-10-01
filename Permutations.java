02171bd3d4457220ed4e654d60b76df50256eccf
Xiangpeng Li <lxp1991@gmail.com>
2017-10-01 15:10:47 -0700

    Permutations

---
+++ Permutations.java
@@ -0,0 +1,29 @@
+public class Solution {
+    /*
+     * @param nums: a list of integers.
+     * @return: a list of permutations.
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        // write your code here
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        // if (nums == null || nums.length == 0) return res;
+        boolean[] isUsed = new boolean[nums.length];
+        helper(res, new ArrayList<Integer>(), nums, isUsed);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, List<Integer> cur, int[] nums, boolean[] isUsed) {
+       if (cur.size() == nums.length) {
+           res.add(new ArrayList<Integer>(cur));
+       }
+
+       for (int i = 0; i < nums.length; i++) {
+           if (isUsed[i]) continue;
+           isUsed[i] = true;
+           cur.add(nums[i]);
+           helper(res, cur, nums, isUsed);
+           isUsed[i] = false;
+           cur.remove(cur.size() - 1);
+       }
+    }
+}
