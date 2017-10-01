acd84b1912d5b70c2bb71e1b5e00fc344a466a1a
Xiangpeng Li <lxp1991@gmail.com>
2017-10-01 14:23:25 -0700

    Plus One Done

---
+++ Plus One.java
@@ -0,0 +1,31 @@
+public class Solution {
+    /*
+     * @param digits: a number represented as an array of digits
+     * @return: the result
+     */
+    public int[] plusOne(int[] digits) {
+        // write your code here
+        if (digits == null || digits.length == 0) {
+            return digits;
+        }
+        int n = digits.length;
+        int[] res = new int[n + 1];
+        // we initialize carry to 1 so it will automatically plus one
+        // we don't need to do any special cases later on
+        int carry = 1;
+        for (int i = n - 1; i >= 0; i--) {
+           res[i + 1] = (carry + digits[i]) % 10;
+           carry = (carry + digits[i]) / 10;
+        }
+        if (carry == 1) {
+            res[0] = 1;
+            return res;
+        } else {
+            int[] noCarry = new int[n];
+            for (int i = 0; i < n; i++) {
+                noCarry[i] = res[i + 1];
+            }
+            return noCarry;
+        }
+    }
+}
