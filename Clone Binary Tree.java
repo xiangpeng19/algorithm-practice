070f8129f40a37818951d1622d396c6e34e49b08
Xiangpeng Li <xiangpeng.li@moodys.com>
2018-03-06 15:15:19 -0800

    finish clone binary tree

---
+++ Clone Binary Tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of binary tree
+     * @return: root of new tree
+     */
+    public TreeNode cloneTree(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(root.val);
+        
+        TreeNode left = cloneTree(root.left);
+        TreeNode right = cloneTree(root.right);
+        node.left = left;
+        node.right = right;
+        return node;
+    }
+
+
+}
