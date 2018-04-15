51cfd2b9085f0b374141e69e87b9fe87c00f0b81
Xiangpeng Li <lxp1991@gmail.com>
2018-04-14 17:55:50 -0700

    new version of pow

--- Pow(x, n).java
+++ Pow(x, n).java
@@ -19,4 +19,21 @@ public class Solution {
             return half * half * x;
         }
     }
+
+
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+        double half = myPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            if (n < 0) {
+                return half * half / x;
+            } else {
+                return half * half * x;
+            }
+        }
+    }
 }
