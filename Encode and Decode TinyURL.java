c46800501240f420e20f9fe8b8433e4f89075372
Xiangpeng Li <xiangpeng.li@moodys.com>
2018-04-19 14:47:42 -0700

    two map to encode tiny url

---
+++ Encode and Decode TinyURL.java
@@ -0,0 +1,39 @@
+public class Codec {
+    
+    final String BASE_URL = "http://tinyurl/";
+    Map<String, String> shortToLong = new HashMap<>();
+    Map<String, String> longToShort = new HashMap<>();
+    String charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int cons = 31;
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        if (longToShort.containsKey(longUrl)) {
+            return BASE_URL + longToShort.get(longUrl);
+        }
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        do {
+            sb = new StringBuilder();
+            for (int i = 0; i < 6; i++) {
+                int index = (random.nextInt(100) * cons) % charSet.length();
+                sb.append(charSet.charAt(index));
+            }
+        } while (longToShort.containsKey(sb.toString()));
+
+        longToShort.put(longUrl, sb.toString());
+        shortToLong.put(sb.toString(), longUrl);
+        return BASE_URL + sb.toString();
+
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        String code = shortUrl.replace(BASE_URL, "");
+        return shortToLong.get(code);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
