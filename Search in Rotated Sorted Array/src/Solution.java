747a7ceaf96d5f98a81e79537bd82ed31b2e4da4
Xiangpeng Li <xiangpeng.li@moodys.com>
2018-04-20 14:30:52 -0700

    search in rotated array

--- Search in Rotated Sorted Array/src/Solution.java
+++ Search in Rotated Sorted Array/src/Solution.java
@@ -10,6 +10,38 @@ import java.util.List;
  */
 public class Solution {
 
+
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[start] <= nums[mid]) {
+                if (target <= nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (nums[mid] <= nums[end]) {
+                if (target <= nums[end] && target >= nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+
+    }
+
     public void heapSort(int[] A) {
         for (int i = 0; i < A.length; i++) {
             buildHeap(A, A.length - i);
