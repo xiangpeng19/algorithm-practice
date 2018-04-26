6cd81ffa29c0bb2caf0015ef79bc8dfe3897531e
Xiangpeng Li <lxp1991@gmail.com>
2018-04-25 20:38:07 -0700

    find sum

---
+++ FindSum.java
@@ -0,0 +1,28 @@
+class Solution {
+
+
+    public boolean findSum(int[] nums, int target) {
+        if (nums == null && nums.length == 0) {
+            return target == 0;
+        }
+
+        int start = 0, sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            while (sum > target) {
+                sum -= nums[start];
+                start++;
+            }
+
+            if (sum == target) {
+                return true;
+            }
+
+        }
+
+
+        return false;
+
+    }
+}
