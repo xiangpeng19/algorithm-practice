820c9964a0c8a7136d9c41409c7f0a55537f2ac9
Xiangpeng Li <lxp1991@gmail.com>
2018-04-27 21:27:32 -0700

    backtracking to find the permutation of 1?1

---
+++ Replace Question Mark.java
@@ -0,0 +1,38 @@
+public class Solution {
+
+
+    public List<String> replaceQuestionMark(String input) {
+        List<String> res = new ArrayList<>();
+        if (input == null || input.length() == 0) {
+            return res;
+        }
+
+        helper(res, input.toCharArray(), "", 0);
+        return res;
+    }
+
+
+    private void helper(List<String> res, char[] arr, String cur, int index) {
+        if (cur.length() == arr.length) {
+            res.add(cur);
+            return;
+        }
+
+        for (int i = index; i < arr.length; i++) {
+            if (arr[i] == '?') {
+                cur += "1";
+                helper(res, arr, cur, i + 1);
+                cur = cur.substring(0, cur.length() - 1);
+                cur += "0";
+                helper(res, arr, cur, i + 1);
+                cur = cur.substring(0, cur.length() - 1);
+            } else {
+                cur += arr[i];
+                helper(res, arr, cur, i + 1);
+                cur = cur.substring(0, cur.length() - 1);
+            }
+
+        }
+    }
+
+}
