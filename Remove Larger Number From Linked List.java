c0baa0f3e86fa03794ea9e9d0fed59182ed76e59
Xiangpeng Li <lxp1991@gmail.com>
2018-05-06 15:10:30 -0700

    remove nodes which values are greater than target in linked list

---
+++ Remove Larger Number From Linked List.java
@@ -0,0 +1,33 @@
+
+class Solution {
+
+
+    public ListNode removeLargerNumbers(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = head;
+
+        while (cur != null) { 
+            while (cur != null && cur.val < k) {
+                pre = cur;
+                cur = cur.next;
+            }
+            if (cur == null) {
+                break;
+            }
+            // now cur.val > k, pre.val < k
+            // remove cur node
+            pre.next = cur.next;
+            cur = pre.next;
+        }
+
+
+        return dummy.next;
+
+    }
+}
