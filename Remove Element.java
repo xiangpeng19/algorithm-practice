63058a8ec4c180d57398760cfe783a36b7d94655
Xiangpeng Li <lxp1991@gmail.com>
2018-05-06 17:07:20 -0700

    new version of removing elements

--- Remove Element.java
+++ Remove Element.java
@@ -32,4 +32,17 @@ public class Solution {
         A[x] = A[y];
         A[y] = temp;
     }
+
+
+    public int removeElement(int[] nums, int val) {
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[p] = nums[i];
+                p++;
+            }
+        }
+        return p;
+        
+    }
 }
