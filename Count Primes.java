b1c1573b8bf5d3206ac17b70ea5f7697e7a71f5a
Xiangpeng Li <lxp1991@gmail.com>
2018-05-15 19:38:48 -0700

    Sieve of Eratosthenes to count the number of primes

---
+++ Count Primes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPrimes(int n) {
+        int counter = 0;
+
+        if (n < 3) {
+            return counter;
+        }
+
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                counter++;
+            }
+
+            for (int j = 1; j * i < n; j++) {
+                isPrime[i * j] = false;
+            }
+
+        }
+
+        return counter;        
+    }
+}
