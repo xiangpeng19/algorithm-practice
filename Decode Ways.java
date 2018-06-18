957d8a05f3dbdf3111a33c950701441231cdb266
Xiangpeng Li <xiangpeng.li@moodys.com>
2018-06-18 16:23:23 -0700

    revisited decode ways

--- Decode Ways.java
+++ Decode Ways.java
@@ -22,5 +22,27 @@ public class Solution {
         }
         return dp[s.length()];
     }
+
+
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        f[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+            int second = Integer.valueOf(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+                f[i] += f[i - 1];
+            }
+            if (second >= 10 && second <= 26) {
+                f[i] += f[i - 2];
+            }
+        }
+        return f[n];
+    }
     
 }
