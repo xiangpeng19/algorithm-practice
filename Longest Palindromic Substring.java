68d933db5b632dcde6ddc16ec5d5b6dbc6d71a4f
Xiangpeng Li <lxp1991@gmail.com>
2018-07-07 18:32:32 -0700

    longest palindrome substring

---
+++ Longest Palindromic Substring.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        int len = s.length();
+        boolean f[][] = new boolean[len][len];
+        int max = 0;
+        String res = null;
+
+        for (int k = 0; k < len; k++) {
+            for (int i = 0; i < len - k; i++) {
+                int j = i + k;
+                if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || f[i + 1][j - 1])) {
+                    f[i][j] = true;
+
+                    if (j - i + 1 > max) {
+                        max = j - i + 1;
+                        res = s.substring(i, j + 1);
+                    }
+
+                }
+
+            }
+        }
+        return res;
+    }
+}
