7d2eb197556015c035afdd4da2ac822548402485
Xiangpeng Li <lxp1991@gmail.com>
2018-07-10 20:43:26 -0700

    minheap/maxheap to solve kth largest number

---
+++ Kth Largest Element in an Array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> y - x);
+        for (int num : nums) {
+            pq.offer(num);
+        }
+
+        while (k > 1) {
+            pq.poll();
+            k--;
+        }
+
+        return pq.poll();        
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            pq.offer(num);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        return pq.peek();        
+    }
+}
