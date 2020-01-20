c3e2c43cb552434ef8f47f422d6032aa470dfd4b
Xiangpeng Li <lxp1991@gmail.com>
2020-01-19 16:40:47 -0800

    Defanging an IP Address

---
+++ Defanging an IP Address.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        if (address == null || address.length() == 0) {
+            return "";
+        }
+
+        String[] blocks = address.split("[.]");
+
+        return String.join("[.]", blocks);
+    }
+}
